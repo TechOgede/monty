3f99171571567da65e5f4dd488f0b08d2f93f82d
TechOgede <sammonius@live.com>
2023-03-24 07:51:20 -0700

    fixed the line number display

--- monty.c
+++ monty.c
@@ -43,6 +43,7 @@ int main(int argc,  char **argv)
 			fprintf(stderr, "malloc failed\n");
 			exit(EXIT_FAILURE);
 		}
+		printf("%d, %s", glob.line_num, glob.buf);
 		args[0] = strtok(glob.buf, "\n\t$ ");
 		args[1] = strtok(NULL, "\n\t$ ");
 		glob.arg = args[1];
