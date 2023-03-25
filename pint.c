53ff71e3b96fa7b44f91d96f5d82a3acb55325ce
TechOgede <sammonius@live.com>
2023-03-25 01:40:04 -0700

    fixed error message

--- pint.c
+++ pint.c
@@ -12,7 +12,7 @@ void pint(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't print, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		free_glob();
 		exit(EXIT_FAILURE);
 	}
