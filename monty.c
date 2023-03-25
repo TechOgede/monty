85d4364b302da9a1a36e4145580d155cc0491c4b
TechOgede <sammonius@live.com>
2023-03-25 01:14:48 -0700

    added capability of reading lines with CL prompt alone

--- monty.c
+++ monty.c
@@ -14,7 +14,7 @@ void init_global_vars(FILE *fp)
 	glob.stack = 1;
 	glob.head = NULL;
 	glob.fp = fp;
-	glob.line_num = 1;
+	glob.line_num = 0;
 }
 /**
 * main - Entry point
@@ -28,30 +28,31 @@ int main(int argc,  char **argv)
 {
 	FILE *fp;
 	void (*op_func)(stack_t **stack, unsigned int line_number);
-	char **args;
 	int  check;
 	size_t size;
 
 	fp = check_file(argc, argv);
 	init_global_vars(fp);
-	args = malloc(sizeof(char *) * 2);
-	if (!args)
+	glob.args_arr = malloc(sizeof(char *) * 2);
+	if (!glob.args_arr)
 	{
 		fclose(fp);
-		fprintf(stderr, "malloc failed\n");
+		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 	while ((check = _getline(&glob.buf, &size, fp)) != -1)
 	{
+		glob.line_num++;
 		if (_strlen(glob.buf) == 1)
 			continue;
-		args[0] = strtok(glob.buf, "\n\t$ ");
-		args[1] = strtok(NULL, "\n\t$ ");
-		glob.arg = args[1];
-		glob.args_arr = args;
-		op_func = opc(args[0], glob.line_num);
-		op_func(&glob.head, glob.line_num);
-		glob.line_num++;
+		glob.args_arr[0] = strtok(glob.buf, "\n\t$ ");
+		glob.args_arr[1] = strtok(NULL, "\n\t$ ");
+		if (glob.args_arr[0])
+		{
+			glob.arg = glob.args_arr[1];
+			op_func = opc(glob.args_arr[0], glob.line_num);
+			op_func(&glob.head, glob.line_num);
+		}
 	}
 	free_glob();
 	return (0);
